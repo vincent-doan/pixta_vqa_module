ebe5b78531eedac0e3a0a07149fe8c5da26624e0
Vincent-Doan <vinh.dt210940@sis.hust.edu.vn>
2024-04-03 09:01:35 +0000

    dtv/feat(run_service) adapt shell with cli args

--- run_service.sh
+++ run_service.sh
@@ -1,3 +1,9 @@
-CUDA_VISIBLE_DEVICES=1 \
-HF_HOME=/home/vinhdoanthe/.cache/huggingface \
-python3 -m uvicorn vqa_module.main:app --host 192.168.100.141 --port 2503 --reload
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <host> <port>"
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=1
+export HF_HOME=/home/vinhdoanthe/.cache/huggingface 
+
+python3 -m uvicorn vqa_module.main:app --host "$1" --port "$2" --reload
