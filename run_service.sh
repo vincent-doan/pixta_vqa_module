d099f288954e48031de0d003bb03788aa64cd2ef
Vincent-Doan <vinh.dt210940@sis.hust.edu.vn>
2024-04-09 02:11:41 +0000

    dtv/feat(run_service) add cli arg for cuda device

--- run_service.sh
+++ run_service.sh
@@ -1,9 +1,9 @@
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <host> <port>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <cuda_device_id> <host> <port>"
     exit 1
 fi
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES="$1"
 export HF_HOME=/home/vinhdoanthe/.cache/huggingface 
 
-python3 -m uvicorn vqa_module.main:app --host "$1" --port "$2" --reload
+python3 -m uvicorn vqa_module.main:app --host "$2" --port "$3" --reload
