943379f289857b1fda2921eba5689f8180ae4156
Vincent-Doan <vinh.dt210940@sis.hust.edu.vn>
2024-04-15 06:33:44 +0000

    update shell script

--- run_service.sh
+++ run_service.sh
@@ -1,9 +1,8 @@
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <cuda_device_id> <host> <port>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: <host> <port>"
     exit 1
 fi
 
-export CUDA_VISIBLE_DEVICES="$1"
 export HF_HOME=/home/vinhdoanthe/.cache/huggingface 
 
-python3 -m uvicorn vqa_module.main:app --host "$2" --port "$3" --reload
+python3 -m uvicorn vqa_module.main:app --host "$1" --port "$2" --reload
